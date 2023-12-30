5a2065e4976ac6d2f850b0ccca8a2065f3d6b42e
RyanVictorDev <vryan8294@gmail.com>
2023-12-29 23:20:37 -0300

    Iniciando a Navbar do projeto usando o router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavbarComponent/>
   <router-view/>
 </template>
 
+<script>
+  import NavbarComponent from './components/NavbarComponent.vue';
+
+  export default{
+    components: {
+    NavbarComponent
+  }
+  }
+</script>
+
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav>
+        <div class="logo">
+            <img src="../assets/tauRosa.png" alt="Tau Rosa">
+            <p>Jovens Sarados</p>
+        </div>
+
+        <div class="links">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/eventos">Eventos</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavbarComponent',
+
+}
+</script>
+
+<style scoped>
+    nav{
+        display: flex;
+        background-color: #102542;
+        justify-content: space-between;
+        padding: 10px;
+        padding-left: 3%;
+        padding-right: 3%;
+    }
+
+    .logo{
+        display: flex;
+        width: 25%;
+    }
+
+    img{
+        padding-right: 2%;
+    }
+
+    p{
+        margin-top: auto;
+        margin-bottom: auto;
+        color: gainsboro;
+    }
+
+    .links{
+        margin-top: auto;
+        margin-bottom: auto;
+    }
+</style>
